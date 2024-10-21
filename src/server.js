101409fe56040979b015f156408c9a1a237c3c1b
Sulu Edward Julianto <sulujulianto@gmail.com>
2024-10-21 21:11:08 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,12 +5,17 @@ const init = async () => {
     const server = Hapi.server({
         port: 9000,
         host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
     });
 
     server.route(routes);
 
     await server.start();
-    console.log('Server berjalan pada %s', server.info.uri);
+    console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
