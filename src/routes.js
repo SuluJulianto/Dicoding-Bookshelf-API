c93ba1b841c519d231981949cabf3eb0dee6b56b
Sulu Edward Julianto <sulujulianto@gmail.com>
2024-10-22 09:15:16 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,31 @@
-const { 
-    addBookHandler, 
-    getAllBooksHandler, 
-    getBookByIdHandler, 
-    editBookByIdHandler, 
-    deleteBookByIdHandler 
-} = require('./handler');
+const { addBook, getAllBooks, getBookById, editBookById, deleteBookById } = require('./handler');
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/books',
-        handler: addBookHandler,
-    },
-    {
-        method: 'GET',
-        path: '/books',
-        handler: getAllBooksHandler,
-    },
-    {
-        method: 'GET',
-        path: '/books/{id}',
-        handler: getBookByIdHandler,
-    },
-    {
-        method: 'PUT',
-        path: '/books/{id}',
-        handler: editBookByIdHandler,
-    },
-    {
-        method: 'DELETE',
-        path: '/books/{id}',
-        handler: deleteBookByIdHandler,
-    },
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBook,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooks,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookById,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookById,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookById,
+  },
 ];
 
 module.exports = routes;
