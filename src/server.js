9eab5f371153296a39890914273bcefd645fbbd2
Sulu Edward Julianto <sulujulianto@gmail.com>
2024-10-22 07:23:58 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,18 +4,18 @@ const routes = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         port: 9000,
-        host: 'localhost',
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-        },
+        host: 'localhost'
     });
 
     server.route(routes);
 
     await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+    console.log(`Server running on ${server.info.uri}`);
 };
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
 
 init();
